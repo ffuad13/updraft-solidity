9daf31d171d7eeca5611c44a50d7fcf0f7a6cb30
ffuad13 <jhonatom99@gmail.com>
2024-11-08 10:50:32 +0800

    init fundme

---
+++ contracts/FundMe.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    function fund() public payable  {
+        require(msg.value > 0.000001e18, "Didn't send enought ETH");
+    }
+
+    function getPrice() public view returns(uint256){
+        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+    function getConversionRate() public {}
+
+    // function withdraw() public {}
+}
