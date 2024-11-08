fd560bcd990b11612a69fc8f5cd5f8a6f543ba88
ffuad13 <jhonatom99@gmail.com>
2024-11-08 11:00:45 +0800

    update fund function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,8 +5,10 @@ pragma solidity 0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+    uint256 public minUsd = 5e18;
+
     function fund() public payable  {
-        require(msg.value > 0.000001e18, "Didn't send enought ETH");
+        require(getConversionRate(msg.value) >= minUsd, "Didn't send enought ETH");
     }
 
     function getPrice() public view returns(uint256){
@@ -15,7 +17,14 @@ contract FundMe {
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 1e10);
     }
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 
     // function withdraw() public {}
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
